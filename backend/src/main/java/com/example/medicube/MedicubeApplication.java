c451061ca37a147b6910bdc4ac7b567d3b174191
Poornima3537 <poornima_dronavalli@srmap.edu.in>
2026-05-23 20:10:23 +0530

    initial setup

---
+++ backend/src/main/java/com/example/medicube/MedicubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicubeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/medicube/MedicubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
